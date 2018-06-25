0599f040cb472b747d66e56c7088dc2ddb2786e7
Ali FRAD <afrad@spark.local>
2018-06-25 11:14:34 +0100

    fixed headers problem

--- src/app/services/contact/contact.service.ts
+++ src/app/services/contact/contact.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {map} from 'rxjs/operators';
 import {Contact} from '../../models/contact.modal';
 
@@ -9,7 +9,6 @@ import {Contact} from '../../models/contact.modal';
 })
 export class ContactService {
   token: string;
-  options: any;
 
 
   constructor(public http: HttpClient) {
@@ -17,47 +16,51 @@ export class ContactService {
 
   getContacts(keyword: String, page: number, size: number): any {
     return this.http.get('http://localhost:8090/searchContacts?keywords=' + keyword + '&size=' + size + '&page=' + page
-      , {headers: this.getHeaders()})
+      , this.getHeaders())
       .pipe(
         map(resp => resp));
   }
 
   addContact(contact: Contact): any {
-    return this.http.post('http://localhost:8090/contacts', contact)
+    return this.http.post('http://localhost:8090/contacts', contact, this.getHeaders())
       .pipe(
         map(resp => resp));
   }
 
   getContact(id: number): any {
-    return this.http.get('http://localhost:8090/contacts/' + id)
+    return this.http.get('http://localhost:8090/contacts/' + id, this.getHeaders())
       .pipe(
         map(resp => resp));
   }
 
   getAllContacts() {
-    return this.http.get('http://localhost:8090/contacts')
+    return this.http.get('http://localhost:8090/contacts', this.getHeaders())
       .pipe(
         map(resp => resp));
   }
 
   updateContact(contact: Contact): any {
-    return this.http.put('http://localhost:8090/contacts/' + contact.id, contact)
+    return this.http.put('http://localhost:8090/contacts/' + contact.id, contact, this.getHeaders())
       .pipe(
         map(resp => resp));
   }
 
   deleteContact(id: number): any {
-    return this.http.delete('http://localhost:8090/contacts/' + id)
+    return this.http.delete('http://localhost:8090/contacts/' + id, this.getHeaders())
       .pipe(
         map(resp => resp));
   }
 
   getHeaders(): any {
-    const headers = new Headers();
     this.token = localStorage.getItem('token');
     console.log('Auth Service token: ' + this.token);
-    headers.append('Content-Type', 'application/json');
-    headers.append('authentication', this.token);
-    return headers;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.token
+      })
+    };
+    console.log(httpOptions);
+    return httpOptions;
   }
 }
